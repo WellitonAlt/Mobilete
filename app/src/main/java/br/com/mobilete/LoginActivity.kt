594157af9a361def1547be3409d158a91a175b1c
Welliton Altimari <etoalt@hotmail.com>
2018-10-18 22:59:25 -0300

    Login com o Facebook

---
+++ app/src/main/java/br/com/mobilete/LoginActivity.kt
@@ -0,0 +1,111 @@
+package br.com.mobilete
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FacebookAuthProvider
+import java.util.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
+
+
+
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "LoginFacebook"
+        const val USUARIO: String = "Usuario"
+    }
+
+    private var mCallbackManager: CallbackManager? = null
+    private var mAuth: FirebaseAuth? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FacebookSdk.sdkInitialize(applicationContext)
+        setContentView(R.layout.activity_login)
+
+        // Inicializa o Firebase Auth
+        mAuth = FirebaseAuth.getInstance()
+
+
+        // Inicializa Botao do Facebook
+        mCallbackManager = CallbackManager.Factory.create()
+        val btnFbLogin = findViewById<LoginButton>(R.id.btnFbLogin)
+
+        btnFbLogin.setOnClickListener {
+            logaFb()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth?.currentUser
+        if (currentUser != null)
+            goToMainActivity(currentUser)
+    }
+
+    private fun goToMainActivity(user: FirebaseUser?){
+        val goToMain= Intent(this, MainActivity::class.java)
+        goToMain.putExtra(USUARIO, user)
+        startActivity(goToMain)
+        finish()
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token)
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sucesso
+                    Log.d(TAG, "signInWithCredential:success")
+                    var user = mAuth!!.currentUser
+                    goToMainActivity(user)
+                } else {
+                    // Fail
+                    Log.w(TAG, "signInWithCredential:failure", task.getException())
+                }
+            }
+    }
+
+    private fun logaFb() {
+        mCallbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().logOut()
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email", "user_friends"))
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d(TAG, "Facebook token: " + loginResult.accessToken.token)
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "Facebook onCancel.")
+
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "Facebook onError. $error")
+
+                }
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mCallbackManager!!.onActivityResult(requestCode, resultCode, data)
+    }
+}
